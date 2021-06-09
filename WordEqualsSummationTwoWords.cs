be2c37fcd6c94fc2c9f618baa8e5fa80c800baec
Mansur Kurtov <kurtov.mansur@gmail.com>
2021-06-09 10:34:56 +0500

    Add files via upload

---
+++ WordEqualsSummationTwoWords.cs
@@ -0,0 +1,23 @@
+﻿namespace MyLeetCodeSulutions
+{
+    public class WordEqualsSummationTwoWords
+    {
+        public bool IsSumEqual(string firstWord, string secondWord, string targetWord)
+        {
+            var a = GetAsciiSum(firstWord);
+            var b = GetAsciiSum(secondWord);
+            var c = GetAsciiSum(targetWord);
+            return a + b == c;
+        }
+        private int GetAsciiSum(string s)
+        {
+            var d = 0;
+            foreach (var item in s)
+            {
+                var code = (int)item - 97;
+                d = d * 10 + code;
+            }
+            return d;
+        }
+    }
+}
