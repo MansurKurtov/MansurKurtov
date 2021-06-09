06e8f0c37ab6ff32d70795cb5fab23b4c111aed5
Mansur Kurtov <kurtov.mansur@gmail.com>
2021-06-09 11:00:47 +0500

    Add files via upload
    Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet

---
+++ ExcelSheetColumnTitle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyLeetCodeSulutions
+{
+    /// <summary>
+    /// Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+    /// 
+    /// For example:
+    /// A -> 1
+    /// B -> 2
+    /// C -> 3
+    /// ...
+    /// Z -> 26
+    /// AA -> 27
+    /// AB -> 28 
+    /// </summary>
+
+    public class ExcelSheetColumnTitle
+    {
+        public string ConvertToTitle(int columnNumber)
+        {
+            var a = columnNumber;
+            var result = String.Empty;
+            var b = 0;
+            while (a > 0)
+            {
+                b = (a - 1) % 26;
+                result = (char)(65 + b) + result;
+                a = (int)((a - b) / 26);
+            }
+            return result;
+        }
+    }
