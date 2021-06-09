0e79600ee75341a5cf3c3563e9acb6c52d861a1e
Mansur Kurtov <kurtov.mansur@gmail.com>
2021-06-09 10:40:24 +0500

    Add files via upload

---
+++ SubrectangleQueries.cs
@@ -0,0 +1,36 @@
+﻿namespace MyLeetCodeSulutions
+{
+    /// <summary>
+    /// Implement the class SubrectangleQueries which receives 
+    /// a rows x cols rectangle as a matrix of integers in the constructor and supports two methods:
+    /// 1. updateSubrectangle(int row1, int col1, int row2, int col2, int newValue)
+    /// Updates all values with newValue in the subrectangle whose upper 
+    /// left coordinate is (row1, col1) and bottom right coordinate is (row2, col2).
+    /// 2. getValue(int row, int col)
+    /// Returns the current value of the coordinate(row, col) from the rectangle.
+    /// </summary>
+    public class SubrectangleQueries
+    {
+        int[][] rectangle;
+        public SubrectangleQueries(int[][] rectangle)
+        {
+            this.rectangle = rectangle;
+        }
+
+        public void UpdateSubrectangle(int row1, int col1, int row2, int col2, int newValue)
+        {
+            for (int i = row1; i <= row2; i++)
+            {
+                for (int j = col1; j <= col2; j++)
+                {
+                    this.rectangle[i][j] = newValue;
+                }
+            }
+        }
+
+        public int GetValue(int row, int col)
+        {
+            return this.rectangle[row][col];
+        }
+    }
+}
