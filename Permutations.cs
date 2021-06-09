a7641a1a775b69e202388f41404db3f66f6bd961
Mansur Kurtov <kurtov.mansur@gmail.com>
2021-06-09 10:43:55 +0500

    Add files via upload
    Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.

---
+++ Permutations.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace MyLeetCodeSulutions
+{
+    /// <summary>
+    /// Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+    /// Example:
+    /// Input: nums = [1,2,3]
+    /// Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+    /// </summary>
+    public class Permutations
+    {
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            var list = new List<IList<int>>();
+            return DoPermute(nums, 0, nums.Length - 1, list);
+        }
+        private IList<IList<int>> DoPermute(int[] nums, int start, int end, IList<IList<int>> list)
+        {
+            if (start == end)
+            {
+                list.Add(new List<int>(nums));
+            }
+            else
+            {
+                for (var i = start; i <= end; i++)
+                {
+                    Swap(ref nums[start], ref nums[i]);
+                    DoPermute(nums, start + 1, end, list);
+                    Swap(ref nums[start], ref nums[i]);
+                }
+            }
+
+            return list;
+        }
+
+        private void Swap(ref int a, ref int b)
+        {
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
